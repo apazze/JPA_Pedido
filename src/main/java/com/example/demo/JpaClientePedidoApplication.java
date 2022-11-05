47756d01b5a30d998b982b5f6cfd6f28d92163e0
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-11-05 14:22:45 -0300

    Uso da CommandLineRunner para testar as classes após o carregamento do Spring

--- src/main/java/com/example/demo/JpaClientePedidoApplication.java
+++ src/main/java/com/example/demo/JpaClientePedidoApplication.java
@@ -1,17 +1,39 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import java.math.BigDecimal;
 
 @SpringBootApplication
-public class JpaClientePedidoApplication {
+public class JpaClientePedidoApplication implements CommandLineRunner {
 
+    public JpaClientePedidoApplication(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository, ClienteRepository clienteRepository, PedidoRepository pedidoRepository, ItemPedidoRepository itemPedidoRepository) {
+        this.categoriaRepository = categoriaRepository;
+        this.produtoRepository = produtoRepository;
+        this.clienteRepository = clienteRepository;
+        this.pedidoRepository = pedidoRepository;
+        this.itemPedidoRepository = itemPedidoRepository;
+    }
+
+    private final CategoriaRepository categoriaRepository;
+    private final ProdutoRepository produtoRepository;
+    private final ClienteRepository clienteRepository;
+    private final PedidoRepository pedidoRepository;
+    private final ItemPedidoRepository itemPedidoRepository;
     public static void main(String[] args) {
         SpringApplication.run(JpaClientePedidoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria = categoriaRepository.save(new Categoria("categoria"));
+        Produto produto = produtoRepository.save(new Produto("nomeProduto", "descricaoProduto", new BigDecimal(10.0), categoria));
+        Cliente cliente = clienteRepository.save(new Cliente("nome", "cpf"));
+
+
+        Pedido pedido = pedidoRepository.save(new Pedido(cliente));
+        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+    }
 }
